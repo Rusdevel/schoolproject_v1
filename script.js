c466e88790458fe23c601d62e5ae32d7cd94caae
Ruslan <ruslanbestaev77@yandex.ru>
2020-04-13 21:44:39 +0300

    Add files

---
+++ script.js
@@ -0,0 +1,155 @@
+flat_12 = {
+    answer: 20,
+    flat: 'Комната 12'
+}
+
+flat_103 = {
+    answer: 21,
+    flat: 'Комната 103'
+}
+
+flat_105 = {
+    answer: 9,
+    flat: 'Комната 105'
+}
+
+flat_107 = {
+    answer: 5,
+    flat: 'Комната 107'
+}
+
+flat_109 = {
+    answer: 3,
+    flat: 'Комната 109' 
+}
+
+flat_28 = {
+    answer: 15237,
+    flat: 'Комната 28'
+}
+
+flat_208 = {
+    answer: 100,
+    flat: 'Комната 208'
+}
+
+flat_210 = {
+    answer: 8432100,
+    flat: 'Комната 210'
+}
+
+flat_212 = {
+    answer: 20,
+    flat: 'Комната 212'
+}
+
+flat_214 = {
+    answer: 34,
+    flat: 'Комната 214'
+}
+
+flat_44 = {
+    answer: 28,
+    flat: 'Комната 44'
+}
+
+flat_313 = {
+    answer: 44,
+    flat: 'Комната 313'
+}
+
+flat_315 = {
+    answer: 45,
+    flat: 'Комната 315'
+}
+
+flat_317 = {
+    answer: 10,
+    flat: 'Комната 317'
+}
+
+flat_319 = {
+    answer: 5,
+    flat: 'Комната 319'
+}
+
+flat_60 = {
+    answer: 90,
+    flat: 'Комната 60'
+}
+
+flat_418 = {
+    answer: 15,
+    flat: 'Комната 418'
+}
+
+flat_420 = {
+    answer: 48,
+    flat: 'Комната 420'
+}
+
+flat_422 = {
+    answer: 10,
+    flat: 'Комната 422'
+}
+
+flat_424 = {
+    answer: 66,
+    flat: 'Комната 424'
+}
+
+flat_76 = {
+    answer: 5,
+    flat: 'Комната 76'
+}
+
+flat_523 = {
+    answer: 175,
+    flat: 'Комната 523'
+}
+
+flat_525 = {
+    answer: 211,
+    flat: 'Комната 525'
+}
+
+flat_527 = {
+    answer: 60,
+    flat: 'Комната 527'
+}
+
+flat_529 = {
+    answer: 20,
+    flat: 'Комната 529'
+}
+
+let questions = [flat_12, flat_103, flat_105, flat_107, flat_109, 
+                 flat_28, flat_208, flat_210, flat_212, flat_214, 
+                 flat_44, flat_313, flat_315, flat_317, flat_319, 
+                 flat_60, flat_418, flat_420, flat_422, flat_424, 
+                 flat_76, flat_523, flat_525, flat_527, flat_529];
+
+let page = document.querySelector('h2');
+
+
+
+
+
+
+
+      
+
+      /* Образец прошлого кода на примере комнаты 102
+      <Script>let Button = document.querySelector ('button');
+    console.log(Button);
+    Button.onclick = function() {
+    let answer = prompt('Ваш ответ', '');
+    if(answer === '44'){
+        document.location.href = "22.html";
+    } else {
+        document.location.href = "103.htmll"; 
+    }
+    }
+    </Script>
+    */
+   
